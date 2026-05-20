5b90616f2352ebd88768328009a80f3648c32e00
Uon1ra <120707623+Uonlra@users.noreply.github.com>
2026-05-20 23:02:21 +0800

    Add skills to profile card and styles

--- React/react-vibe-learning/stage-01-profile-card/src/main.jsx
+++ React/react-vibe-learning/stage-01-profile-card/src/main.jsx
@@ -4,7 +4,7 @@ import "./style.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <StrictMode>  {/*StrictMode 是 React 的一个工具，用于帮助开发者发现潜在的问题和不安全的生命周期方法。它不会渲染任何 UI，但会在开发模式下激活额外的检查和警告。 */}
     <App />
   </StrictMode>
 );
