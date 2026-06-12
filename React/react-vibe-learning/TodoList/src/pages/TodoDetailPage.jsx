c0694771f6b66d4b01630e2c2ac42e398f1e24f2
Uon1ra <120707623+Uonlra@users.noreply.github.com>
2026-06-12 12:48:37 +0800

    feat: 新增 Todo 详情页

---
+++ React/react-vibe-learning/TodoList/src/pages/TodoDetailPage.jsx
@@ -0,0 +1,40 @@
+import { Link, useParams } from "react-router-dom";
+import useTodos from "../hooks/useTodos.js";
+
+export default function TodoDetailPage() {
+  const { todoId } = useParams();
+  const { todos } = useTodos();
+  const todo = todos.find((item) => String(item.id) === todoId);
+
+  if (!todo) {
+    return (
+      <section className="route-page">
+        <h1>没有找到这个任务</h1>
+        <p>这个任务可能已经被删除，或者当前地址里的任务 ID 不存在。</p>
+        <Link className="primary-link" to="/todos">
+          返回任务列表
+        </Link>
+      </section>
+    );
+  }
+
+  return (
+    <section className="route-page">
+      <p className="detail-label">任务详情</p>
+      <h1>{todo.text}</h1>
+      <dl className="todo-detail-list">
+        <div>
+          <dt>任务 ID</dt>
+          <dd>{todo.id}</dd>
+        </div>
+        <div>
+          <dt>完成状态</dt>
+          <dd>{todo.completed ? "已完成" : "未完成"}</dd>
+        </div>
+      </dl>
+      <Link className="primary-link" to="/todos">
+        返回任务列表
+      </Link>
+    </section>
+  );
+}
